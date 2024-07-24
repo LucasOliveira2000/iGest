01356a9f6bf9443c48067cedb4ba0f23c72332a3
LucasOliveira2000 <j-lucas9@hotmail.com>
2024-07-23 22:33:12 -0300

    provider
    provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,21 @@ class AppServiceProvider extends ServiceProvider
     {
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
+
+            // Adiciona cabeçalhos para arquivos estáticos
+            \Illuminate\Support\Facades\Response::macro('css', function ($path) {
+                return response()->file($path, [
+                    'Content-Type' => 'text/css; charset=UTF-8',
+                    'X-Content-Type-Options' => 'nosniff',
+                ]);
+            });
+
+            \Illuminate\Support\Facades\Response::macro('js', function ($path) {
+                return response()->file($path, [
+                    'Content-Type' => 'application/javascript; charset=UTF-8',
+                    'X-Content-Type-Options' => 'nosniff',
+                ]);
+            });
         }
 
     }
