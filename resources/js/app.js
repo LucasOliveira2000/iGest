403cd7da60f8408065ecd26adbd41a327335f2f8
LucasOliveira2000 <j-lucas9@hotmail.com>
2024-07-23 21:47:05 -0300

    css
    css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,8 @@
-import './bootstrap';
-import '../css/app.css';
-
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { Quasar, Notify } from 'quasar';
 import Flowbite from '../../node_modules/floating-vue';
-
-
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
