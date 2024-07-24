4601f98d19898f7bb17be1b8ea41c7cf48e54c04
LucasOliveira2000 <j-lucas9@hotmail.com>
2024-07-24 18:23:14 -0300

    build1
    build2

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         })
-    ]
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: '[name].[hash].[ext]'
+            }
+        }
+    }
 });
